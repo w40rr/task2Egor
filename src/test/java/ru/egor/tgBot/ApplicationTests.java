f54444a7a87ec80d8c55b3320a7cb4503730b319
w40rr <125339519+w40rr@users.noreply.github.com>
2025-07-17 21:14:52 +0300

    Add files via upload

---
+++ src/test/java/ru/egor/tgBot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egor.tgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
